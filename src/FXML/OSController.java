9ee992b76897ef1a8a2f8144c5ec888e7f0ce5ef
Caio de Souza <csm_versao2009@hotmail.com>
2020-03-12 18:51:40 -0300

    Service Order - INSERT

--- src/FXML/OSController.java
+++ src/FXML/OSController.java
@@ -15,11 +15,13 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class OSController implements Initializable {
@@ -38,19 +40,31 @@ public class OSController implements Initializable {
 	private JFXButton procurar;
 
 	@FXML
-	private TextField numero;
+	private TextField numero,txtProcurar;
 
 	@FXML
 	private TextField data1;
 
 	@FXML
-	private TextField txtProcurar;
-	
-	@FXML
 	private TextField id;
 
 	@FXML
-	private ToggleGroup escolhas;
+	private TextField equipamento;
+
+	@FXML
+	private TextField defeito;
+
+	@FXML
+	private TextField servico;
+
+	@FXML
+	private TextField tecnico;
+
+	@FXML
+	private TextField valor;
+	
+	@FXML
+	private ToggleGroup tipo;
 
 	@FXML
 	private ComboBox<String> situacao;
@@ -71,45 +85,94 @@ public class OSController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
 		situacao.setStyle("-fx-text-inner-color: #a0a2ab;");
-		situacao.getItems().addAll("Entrega OK", "Orçamento REPROVADO", "Aguardando Aprovação","Aguardando peças","Abandonado pelo cliente","Na bancada","Retornou");
-		
+		situacao.getItems().addAll("Entrega OK", "Orçamento REPROVADO", "Aguardando Aprovação", "Aguardando peças",
+				"Abandonado pelo cliente", "Na bancada", "Retornou");
+
 		idcli.setCellValueFactory(new PropertyValueFactory<>("idcli"));
 		nomecli.setCellValueFactory(new PropertyValueFactory<>("nomecli"));
 		fonecli.setCellValueFactory(new PropertyValueFactory<>("fonecli"));
-		
-	 try {
-		 String sql = "select * from tbclientes ";	
-		 pst = (PreparedStatement) conexao.prepareStatement(sql);
+
+		try {
+			String sql = "select * from tbclientes ";
+			pst = (PreparedStatement) conexao.prepareStatement(sql);
 			rs = pst.executeQuery();
-	
+
 			while (rs.next()) {
-				listview.add(new TableModel2(rs.getString("idcli"), rs.getString("nomecli"),rs.getString("fonecli")));
+				listview.add(new TableModel2(rs.getString("idcli"), rs.getString("nomecli"), rs.getString("fonecli")));
 			}
 			table.setItems(listview);
-			
+
 		} catch (Exception e) {
 
 		}
 	}
-   
-	 
-	 
+
 	public OSController() {
 		conexao = (Connection) ModuloConexao.conector();
-		
-		}
-	
+
+	}
+
 	@FXML
 	private void setar_campos() {
 		id.setText(table.getSelectionModel().getSelectedItem().getIdcli());
 	}
-	
-	
+
+	public String getTipo() {
+		String esc = "";
+
+		if (orC.isSelected()) {
+			esc = "Orçamento";
+		} else if (odS.isSelected()) {
+			esc = "Ordem de Serviço";
+		}
+		return esc;
+
+	}
+
+	@FXML
+	private void emitir_os(ActionEvent evt1) {
 		
+		String sql = "insert into tbos(tipo,situacao,equipamento,defeito,servico,tecnico,valor,idcli) values(?,?,?,?,?,?,?,?)";
 		
-		
+		try {
+			pst = (PreparedStatement) conexao.prepareStatement(sql);
+			pst.setString(1, getTipo());
+			pst.setString(2, situacao.getSelectionModel().getSelectedItem().toString());
+			pst.setString(3, equipamento.getText());
+			pst.setString(4, defeito.getText());
+			pst.setString(5, servico.getText());
+			pst.setString(6, tecnico.getText());
+			pst.setString(7, valor.getText().replace(",", "."));
+			pst.setString(8, id.getText());
 			
-			
+			if ((id.getText().isEmpty()) ||(equipamento.getText().isEmpty()) || (defeito.getText().isEmpty()) ) {
+            	
+				Alert alert = new Alert(AlertType.INFORMATION , "Preencha todos os campos obrigatórios!");
+				
+            	alert.showAndWait();
+				
+			}else {
+				
+				int adicionado = pst.executeUpdate();
+				
+				if(adicionado > 0) {
+					
+					Alert alert = new Alert(AlertType.INFORMATION , "Ordem de serviço emitida com sucesso!");
+					alert.showAndWait();
+					id.setText(null);
+					equipamento.setText(null);
+					defeito.setText(null);
+					servico.setText(null);
+					tecnico.setText(null);	
+					valor.setText(null);
+				}
+				
+			}
+		} catch (Exception e) {
+
+		}
+	}
+
 }
